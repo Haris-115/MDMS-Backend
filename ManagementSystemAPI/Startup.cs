fea324a9a635b8bd2680705cadfeb7bce12874a9
Haris-115 <hsuhail32@gmail.com>
2022-06-21 23:54:29 +0500

    complete backend

---
+++ ManagementSystemAPI/Startup.cs
@@ -0,0 +1,228 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ManagementSystemAPI.DataAccess;
+using ManagementSystemAPI.DataAccess.Implementation;
+using ManagementSystemAPI.Helpers;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace ManagementSystemAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Zomato API",
+                    Version = "v1",
+                    Description = "Description for the API goes here.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ankush Jain",
+                        Email = string.Empty,
+                        Url = new Uri("https://coderjony.com/"),
+                    },
+                });
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Login",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("GetStocks",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("GetStocksDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("MedstoreDetailsByUserid", 
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    }); 
+                options.AddPolicy("MedstoreDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("GetMedidOrders",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("ManufacturesDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyOrigin()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("OrderItems",
+                     builder =>
+                     {
+                         builder.WithOrigins("*")
+                                             .AllowAnyHeader()
+                                             .AllowAnyOrigin()
+                                             .AllowAnyMethod();
+                     });
+                options.AddPolicy("PlaceOrder", 
+                   builder =>
+                   {
+                       builder.WithOrigins("*")
+                                           .AllowAnyHeader()
+                                           .AllowAnyOrigin()
+                                           .AllowAnyMethod();
+                   });
+                options.AddPolicy("GetDistributors",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("Signup",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("OrderCount",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("DistributorDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("OrderItemsDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    }); 
+                options.AddPolicy("OrderDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+                options.AddPolicy("OrderItemsDetails",
+                    builder =>
+                    {
+                        builder.WithOrigins("*")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+            });
+            var sqlConnectionString = Configuration["PostgreSqlConnectionString"];
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            services.AddDbContext<PostgreSqlContext>(options => options.UseNpgsql(sqlConnectionString));
+
+            services.AddScoped<IDistributorsProvider, DistributorsProvider>();
+            services.AddScoped<IManufacturesProvider, ManufacturesProvider>();
+            services.AddScoped<IMedicinesProvider, MedicinesProvider>();
+            services.AddScoped<IMedstoresProvider, MedstoresProvider>();
+            services.AddScoped<IOrdersProvider, OrdersProvider>();
+            services.AddScoped<IOrderitemsProvider, OrderitemsProvider>();
+            services.AddScoped<IStocksProvider, StocksProvider>();
+            services.AddScoped<IUsersProvider, UsersProvider>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zomato API V1");
+
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
+        }
+    }
+}
